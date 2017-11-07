3beea1adbdd8e644f1c27478e06f27b1c02caab6
Søren Roug <soren.roug@eea.europa.eu>
2017-11-07 09:44:18 +0100

    Works now: mvn -Pdocker clean install

---
+++ src/main/java/eionet/rod/controller/SimplePageController.java
@@ -0,0 +1,61 @@
+package eionet.rod.controller;
+
+import eionet.rod.util.BreadCrumbs;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Controller for simple pages.
+ */
+
+@Controller
+public class SimplePageController {
+
+    /**
+     * Frontpage.
+     *
+     * @param model holder for model attributes
+     * @return view name
+     */
+    @RequestMapping(value = "/")
+    public String frontpage(Model model) {
+        // This is toplevel. No breadcrumbs.
+        BreadCrumbs.set(model);
+        return "index";
+    }
+
+    /**
+     * About.
+     */
+    @RequestMapping(value = "/about")
+    public String about(Model model) {
+        String title = "About";
+        model.addAttribute("title", title);
+        BreadCrumbs.set(model, title);
+        return "about";
+    }
+
+    /**
+     * Redirects to welcome page after login.
+     *
+     * @param model holder for model attributes
+     * @return view name
+     */
+    @RequestMapping(value = "/login")
+    public String login(Model model) {
+        return frontpage(model);
+    }
+
+    /**
+     * Shows page which allows to perform SingleSignOut.
+     *
+     * @return view name
+     */
+    @RequestMapping(value = "/logout")
+    public String logout() {
+        return "logout_all_apps";
+    }
+
+}
