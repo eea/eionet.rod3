a6f218569d8eb3afc3a83418418e7fab4423c9f3
Søren Roug <soren.roug@eea.europa.eu>
2017-11-17 13:40:39 +0100

    Added a unit test.

---
+++ src/test/java/eionet/rod/util/BreadCrumbsTest.java
@@ -0,0 +1,33 @@
+package eionet.rod.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import eionet.rod.model.BreadCrumb;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.springframework.ui.ExtendedModelMap;
+
+public class BreadCrumbsTest {
+
+    @Test
+    public void nullTest() {
+        ExtendedModelMap testModel = new ExtendedModelMap();
+        BreadCrumbs.set(testModel);
+        assertTrue(testModel.containsAttribute("breadcrumbs"));
+        @SuppressWarnings("unchecked")
+        List<BreadCrumb> breadcrumbList = (List<BreadCrumb>) testModel.get("breadcrumbs");
+        assertEquals(1, breadcrumbList.size());
+    }
+
+    @Test
+    public void aboutCrumb() {
+        ExtendedModelMap testModel = new ExtendedModelMap();
+        BreadCrumbs.set(testModel, new BreadCrumb("/about", "About E-PRTR"));
+        assertTrue(testModel.containsAttribute("breadcrumbs"));
+        @SuppressWarnings("unchecked")
+        List<BreadCrumb> breadcrumbList = (List<BreadCrumb>) testModel.get("breadcrumbs");
+        assertEquals(2, breadcrumbList.size());
+    }
+}
