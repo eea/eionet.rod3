fc18e094a0002ec801cb7fc9f329114de60d21cd
BILBOMATICA2\ycarrasco <ycarrasco@bilbomatica.es>
2018-02-28 16:35:51 +0100

    test error

--- src/test/resources/seed-obligation-source.sql
+++ src/test/resources/seed-obligation-source.sql
@@ -175,3 +175,4 @@ INSERT INTO T_DELIVERY (RA_URL, TITLE, UPLOAD_DATE, DELIVERY_URL, TYPE, FORMAT, 
    VALUES ('http://cdr.eionet.europa.eu', 'END - Austria - DF2 - 2015', '2015-11-26', 'http://cdr.eionet.europa.eu', 'P1Y', null, 1, 1, 'Austria', 'only for geometry correction', null);
    
 DELETE FROM T_UNDO;
+ALTER TABLE T_UNDO ALTER COLUMN VALUE VARCHAR(2000);
