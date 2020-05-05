19cfaf41db1d63a793d7733be50193a6e28fae53
Eduardo Alfaro <ealfaro@tracasa.es>
2020-05-05 16:03:06 +0200

    Added frequency reporting and detail to list of obligations.

--- src/main/java/eionet/rod/dao/ObligationsDaoImpl.java
+++ src/main/java/eionet/rod/dao/ObligationsDaoImpl.java
@@ -238,7 +238,7 @@ public class ObligationsDaoImpl implements ObligationsDao {
 
         String query = "SELECT distinct OB.PK_RA_ID AS obligationId, OB.TITLE AS oblTitle, OB.DESCRIPTION AS description, "
                 + "SO.PK_SOURCE_ID as sourceId, SO.TITLE AS sourceTitle, SO.ALIAS as sourceAlias, SO.SOURCE_CODE as sourceCode, "
-                + "OB.NEXT_DEADLINE as nextDeadline, "
+                + "OB.NEXT_DEADLINE as nextDeadline, OB.REPORT_FREQ_DETAIL as reportFreqDetail, OB.REPORT_FREQ as reportFreq, "
                 + "CL.PK_CLIENT_ID as clientId, CL.CLIENT_NAME as clientName, RRO.ROLE_ID AS respRoleId, RRO.ROLE_NAME AS respRoleName, OB.NEXT_REPORTING as nextReporting ";
         if (deadlinePage) {
             query += ", OB.FK_DELIVERY_COUNTRY_IDS REGEXP CONCAT('.',RAS.FK_SPATIAL_ID,'.') AS hasdelivery "
